e3d650ada4d8044468461aea4dacacca7c6c2aa9
Thiago da Silva <thiggasto@gmail.com>
2025-11-21 18:42:16 -0300

    chore: adicionar dotenv, gitignore e criptografia básica

--- MedicareClinic/server/app.js
+++ MedicareClinic/server/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const sequelize = require('./config/database')
-const port = 3000
+const PORT = process.env.PORT || 3000
 const rotasPaciente = require('./routes/pacienteRoutes')
 
 app.use(express.json())
@@ -10,7 +12,7 @@ app.use('/pacientes', rotasPaciente)
 
 sequelize.sync().then(() => {
   console.log('Banco de dados sincronizado.')
-  app.listen(port, () => {
-    console.log(`Servidor rodando em http:127.0.0.1:${port}`)
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http:127.0.0.1:${PORT}`)
   })
 })
--- MedicareClinic/server/config/database.js
+++ MedicareClinic/server/config/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize ({
     dialect: 'sqlite',
-    storage:'./medicare.db'
+    storage: process.env.DB_PATH
 })
 
 module.exports = sequelize;
